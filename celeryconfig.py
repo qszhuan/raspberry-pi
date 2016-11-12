90849f9eeb2360ced64e23c149a19422d8551756
Zhuan Qingshan <zhuanqingshan@163.com>
2016-11-12 21:35:12 +1100

    add solor tasks

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,3 +1,10 @@
+from celery.schedules import crontab
+from celery.schedules import solar
+
+lat_lon = {
+    'melbourne': [-37.8097515, 145.2331785]
+}
+
 broker_url = 'redis://localhost:6379/0'
 result_backend = 'redis://localhost'
 
@@ -12,6 +19,65 @@ task_soft_time_limit = 5
 task_routes = {
     # 'tasks.add': 'default',
     # 'tasks.play': 'media',
+}
+
+beat_schedule = {
+    'add-every-10-seconds': {
+        'task': 'tasks.add',
+        'schedule': 10.0,
+        'args': (16, 16)
+    },
+    'add-every-monday-morning': {
+        'task': 'tasks.add',
+        'schedule': crontab(hour=20, minute=45, day_of_week=6),
+        'args': (100, 16),
+    },
+
+    'record-at-melbourne-dawn_astronomical': {
+        'task': 'tasks.solar',
+        'schedule': solar('dawn_astronomical', *lat_lon['melbourne']),
+        'args': ('dawn_astronomical',),
+    },
+    'record-at-melbourne-dawn_nautical': {
+        'task': 'tasks.solar',
+        'schedule': solar('dawn_nautical', *lat_lon['melbourne']),
+        'args': ('dawn_nautical',),
+    },
+    'record-at-melbourne-dawn_civil': {
+        'task': 'tasks.solar',
+        'schedule': solar('dawn_civil', *lat_lon['melbourne']),
+        'args': ('dawn_civil',),
+    },
+    'record-at-melbourne-sunset': {
+        'task': 'tasks.solar',
+        'schedule': solar('sunset', *lat_lon['melbourne']),
+        'args': ('sunset',),
+    },
+    'record-at-melbourne-sunrise': {
+        'task': 'tasks.solar',
+        'schedule': solar('sunrise', *lat_lon['melbourne']),
+        'args': ('sunrise',),
+    },
+    # 'record-at-melbourne-solar_noon': {
+    #     'task': 'tasks.solar',
+    #     'schedule': solar('solar_noon', *lat_lon['melbourne']),
+    #     'args': ('solar_noon',),
+    # },
+    'record-at-melbourne-dusk_civil': {
+        'task': 'tasks.solar',
+        'schedule': solar('dusk_civil', *lat_lon['melbourne']),
+        'args': ('dusk_civil', ),
+    },
+    'record-at-melbourne-dusk_nautical': {
+        'task': 'tasks.solar',
+        'schedule': solar('dusk_nautical', *lat_lon['melbourne']),
+        'args': ('dusk_nautical', ),
+    },
+    'record-at-melbourne-dusk_astronomical': {
+        'task': 'tasks.solar',
+        'schedule': solar('dusk_astronomical', *lat_lon['melbourne']),
+        'args': ('dusk_astronomical', ),
+    },
 }
 
 task_annotations = {
--- tasks.py
+++ tasks.py
@@ -4,18 +4,44 @@ import shutil
 import random
 from celery.exceptions import SoftTimeLimitExceeded
 import subprocess
+from celery.schedules import crontab
+import datetime
 
 app = Celery('tasks')
 app.config_from_object('celeryconfig')
 
+#
+# @app.on_after_configure.connect
+# def setup_periodic_tasks(sender, **kwargs):
+#     # Calls test('hello') every 10 seconds.
+#     sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+#
+#     # Calls test('world') every 30 seconds
+#     sender.add_periodic_task(30.0, test.s('world'), expires=10)
+#
+#     # Executes every Monday morning at 7:30 a.m.
+#     sender.add_periodic_task(
+#         crontab(hour=7, minute=30, day_of_week=1),
+#         test.s('Happy Mondays!'),
+#     )
+
 
 @app.task
-def add(x, y):
-    return x + y
+def solar(event):
+    now = datetime.datetime.now()
+    solar_event = '{0} : Event: {1}'.format(now, event)
+    print(solar_event)
+    return solar_event
+
+
+@app.task
+def add(a, b):
+    print('Result is ', a + b)
+    return a + b
 
 
 @app.task(bind=True)
-def play(self):
+def play(self, song_name=""):
     prc = None
     try:
         files = os.listdir('./rings')
@@ -31,7 +57,3 @@ def play(self):
     except SoftTimeLimitExceeded:
         prc.terminate()
         return 'Exceeded soft time limit.'
-
-
-play.delay()
-
