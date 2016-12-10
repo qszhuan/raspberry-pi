1d6b2a8c0c88256732b97b89029f3f9a86ad0c71
Zhuan Qingshan <zhuanqingshan@163.com>
2016-12-10 16:25:50 +1100

    obstacle avoidance sendor tset.py

---
+++ gpio/obstacle-avoidance-sensor-test.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+IN_PIN = 18
+GPIO.setup(IN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+while True:
+          i=GPIO.input(IN_PIN)                        
+          if i==0:                                
+               print "Obstacle detected",i
+               time.sleep(0.1)
