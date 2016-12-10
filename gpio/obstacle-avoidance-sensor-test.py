193856a0363773e8af401617a25d858579434850
Qingshan Zhuan <zhuanqingshan@gmail.com>
2016-12-10 16:52:05 +1100

    add time

--- gpio/obstacle-avoidance-sensor-test.py
+++ gpio/obstacle-avoidance-sensor-test.py
@@ -1,12 +1,14 @@
 import RPi.GPIO as GPIO
 import time
+import datetime
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 IN_PIN = 18
 GPIO.setup(IN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP) 
+
 while True:
           i=GPIO.input(IN_PIN)                        
           if i==0:                                
-               print "Obstacle detected",i
+               print(datetime.datetime.now(), "Obstacle detected",i)
                time.sleep(0.1)
